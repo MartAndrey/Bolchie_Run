4dc4856cba43d517e445f19ceb0a395534a5add3
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-06-27 18:52:41 -0500

    Setting the camera to follow the player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //It is the objective that the camera has to follow
+    public Transform tarjet;
+
+    //Target Tracking Distance
+    public Vector3 offset = new Vector3(0.2f, 0f, -10f);
+
+    //Damping time with which the camera moves
+    public float dampingTime = 0.3f;
+
+    //Camera speed
+    public Vector3 velocity = Vector3.zero;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera(true);
+    }
+
+    public void ResetCameraPosition()
+    {
+        MoveCamera(false);
+    }
+
+    void MoveCamera(bool smooth)
+    {
+        Vector3 destination = new Vector3(tarjet.position.x - offset.x, offset.y, offset.z);
+
+        if (smooth)
+        {
+            this.transform.position = Vector3.SmoothDamp(this.transform.position, destination, ref velocity, dampingTime);
+        }
+        else
+        {
+            this.transform.position = destination;
+        }
+    }
+}
