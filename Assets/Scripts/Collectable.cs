d046fb2e31c6208296cc78f4a018b65af52278f8
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-07-03 12:41:08 -0500

    Collectible items

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CollectableType { HealthPotion, ManaPotion, Money }
+
+public class Collectable : MonoBehaviour
+{
+    public CollectableType type = CollectableType.Money; 
+
+    SpriteRenderer sprite;
+
+    CircleCollider2D itemCollider;
+
+    bool hasBeenCollected = false;
+
+    public int value = 1;
+
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+
+        itemCollider = GetComponent<CircleCollider2D>();
+    }
+
+    void Show()
+    {
+        sprite.enabled = true;
+
+        itemCollider.enabled = true;
+
+        hasBeenCollected = false;
+    }
+
+    void Hide()
+    {
+        sprite.enabled = false;
+
+        itemCollider.enabled = false;
+    }
+
+    void Collect()
+    {
+        Hide();
+
+        hasBeenCollected = true;
+
+        switch (this.type)
+        {
+            case CollectableType.HealthPotion:
+
+                break;
+
+            case CollectableType.ManaPotion:
+
+                break;
+
+            case CollectableType.Money:
+
+                break;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Collect();
+        }
+    }
+}
